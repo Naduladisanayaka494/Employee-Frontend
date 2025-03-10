ee146347495c5127fbf87cd608c8770a70a7c391
Naduladisanayaka494 <disanayakanadula494@gmail.com>
2025-03-10 12:29:38 +0530

    feat employee routes created

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ReportsComponent } from './reports/reports.component';
+import { EployeeDetailsComponent } from './eployee-details/eployee-details.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'reports', component: ReportsComponent },
+  { path: 'details', component: EployeeDetailsComponent },
 ];
 
 @NgModule({
---
+++ src/app/modules/admin/eployee-details/eployee-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eployee-details',
+  templateUrl: './eployee-details.component.html',
+  styleUrls: ['./eployee-details.component.css']
+})
+export class EployeeDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
